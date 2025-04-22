85c3d319674360343a62681ca7d507e090c5a0bd
YakupUzunoglu <105830274+Yakup-uzn@users.noreply.github.com>
2025-04-22 12:57:38 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDFNcp-q0_kmkPZ3gx8NG4X_KgOG2YPYH0",
-  authDomain: "sysguard-f8f4c.firebaseapp.com",
-  projectId: "sysguard-f8f4c",
-  storageBucket: "sysguard-f8f4c.firebasestorage.app",
-  messagingSenderId: "436599639110",
-  appId: "1:436599639110:web:1fd15cca9dd2fbfaf18e47",
-  measurementId: "G-VP0SE7RKHP"
+  apiKey: "***********",
+  authDomain: "***********",
+  projectId: "*************",
+  storageBucket: "*********",
+  messagingSenderId: "*********",
+  appId: "****",
+  measurementId: "******"
 };
 
 // Initialize Firebase
